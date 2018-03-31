c7cdf7cbe00bac8d53d3b12b24e6cb6aed593ace
Ahmed Riza <dr.ahmed_riza@yahoo.co.uk>
2018-03-31 13:49:48 +0100

    IntSet

--- week3/week3/src/main/scala/IntSet.scala
+++ week3/week3/src/main/scala/IntSet.scala
@@ -1,4 +1,5 @@
 
+
 abstract class IntSet {
   def contains(x: Int): Boolean
   def incl(x: Int): IntSet
@@ -9,30 +10,74 @@ abstract class IntSet {
 //   a tree for the empty set
 //   a tree consisting of an integer and two sub trees
 //
-//            10
+//            6
 //           / \
-            /   \
+//          /   \
 //         /     \
-//        5       7
+//        5      11
 //      /  \    /  \
-//     E   E   6    12
+//     E   E   7    12
 //            / \   / \
-//           E  E  E   E
-//
-class Empty extends IntSet {
+//           E  8  E   E
+//             / \
+//            E  9
+//              / \
+//             E  E
+
+/**
+  * Singleton Empty object (only one instance exists).
+  */
+object Empty extends IntSet {
   override def contains(x: Int): Boolean = false
-  override def incl(x: Int): IntSet = new NonEmpty(x, new Empty, new Empty)
+  override def incl(x: Int): IntSet = new NonEmpty(x, Empty, Empty)
+
+  override def toString: String = s"E"
 }
 
 class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet {
 
-  override def contains(x: Int): Boolean = {
+  def contains(x: Int): Boolean = {
     if (x < elem) left contains x
     else if (x > elem) right contains x
     else true
   }
 
-  override def incl(x: Int): IntSet = {
+  /**
+    * Note that this is purely functional and the tree is persistent, i.e. we do not modify the previous tree
+    * when adding new nodes.
+    *
+    * For example, let's say the tree so far is this:
+    *            6
+    *           / \
+    *          /   \
+    *         /     \
+    *        5      11
+    *      /  \    /  \
+    *     E   E   E    E
+    *
+    * If we now call incl(7), here is what happens
+    *
+    *             6    new tree
+    *           /  \
+    *          /    \
+    *         /      \
+    *         |      11
+    *         |     /  \
+    *         |    7    E
+    *         |   / \
+    *         |  E   E      6  old tree
+    *         |            / \
+    *         |           /   \
+    *         |          /     \
+    *         ------->  5      11
+    *                 /  \    /  \
+    *                E   E   E    E
+    *
+    * Note how the old tree is lef unmodified, and the new tree points to the left subtree (starting with node 5) of
+    * the old one, since that branch was not modified and both old and new trees share that same branch.
+    */
+
+  def incl(x: Int): IntSet = {
     if (x < elem) { // include x in left sub-tree
       new NonEmpty(elem, left incl x, right)
     } else if (x > elem) { // include x in right sub-tree
@@ -40,5 +85,25 @@ class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet {
     } else {
       this
     }
+  }
+
+  override def toString: String = s"{$left $elem $right}"
+}
+
+object IntSet {
+
+  def main(args: Array[String]): Unit = {
+
+    val head = Empty.incl(6)
+    val n1 = head.incl(5)
+    val n2 = n1.incl(11)
+    val n3 = n2.incl(7)
+    val n4 = n3.incl(12)
+    val n5 = n4.incl(8)
+    val n6 = n5.incl(9)
+
+    val n7 = n6.incl(7)
+
+    println(n7)
   }
 }
---
+++ week3/week3/src/main/scala/NodeSet.scala
@@ -0,0 +1,91 @@
+
+class Node(private val x: Int, private val msg: String) {
+
+  def this(x: Int) = this(x, x.toString)
+
+  def < (that: Node): Boolean = this.x < that.x
+
+  def > (that: Node): Boolean = this.x > that.x
+
+  override def equals(obj: scala.Any): Boolean = {
+    obj match {
+      case that: Node => this.x == that.x
+      case _ => false
+    }
+  }
+
+  override def hashCode(): Int = 41 * (41 + this.x)
+
+  override def toString: String = s"$x"
+}
+
+abstract class NodeSet {
+  def contains(x: Node): Boolean
+  def incl(x: Node): NodeSet
+}
+
+// Consider implementing sets as binary trees.
+// There are two possible types of trees:
+//   a tree for the empty set
+//   a tree consisting of an integer and two sub trees
+//
+//            6
+//           / \
+//          /   \
+//         /     \
+//        5      11
+//      /  \    /  \
+//     E   E   7    12
+//            / \   / \
+//           E  8  E   E
+//             / \
+//            E  9
+//              / \
+//             E  E
+
+class Leaf extends NodeSet {
+  override def contains(x: Node): Boolean = false
+  override def incl(x: Node): NodeSet = new Element(x, new Leaf, new Leaf)
+
+  // override def toString: String = s"E"
+}
+
+class Element(node: Node, left: NodeSet, right: NodeSet) extends NodeSet {
+
+  println(s"Constructing Element with node: $node, left: $left, right: $right")
+
+  override def contains(x: Node): Boolean = {
+    if (x < node) left contains x
+    else if (x > node) right contains x
+    else true
+  }
+
+  override def incl(x: Node): NodeSet = {
+    if (x < node) { // include x in left sub-tree
+      new Element(node, left incl x, right)
+    } else if (x > node) { // include x in right sub-tree
+      new Element(node, left, right incl x)
+    } else {
+      this
+    }
+  }
+
+  // override def toString: String = s"$elem, left: $left, right: $right"
+}
+
+object NodeSet {
+  def main(args: Array[String]): Unit = {
+    val head = new Leaf().incl(new Node(6))
+    val n1 = head.incl(new Node(5))
+    val n2 = n1.incl(new Node(11))
+
+    val n3 = n2.incl(new Node(7))
+
+    val n4 = n3.incl(new Node(12))
+    val n5 = n4.incl(new Node(8))
+    val n6 = n5.incl(new Node(9))
+
+    val n7 = n6.incl(new Node(7))
+    println()
+  }
+}
