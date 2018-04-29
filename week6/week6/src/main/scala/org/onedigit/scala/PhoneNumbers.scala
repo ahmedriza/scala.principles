754bccf1cc25bcc1e70798213252e46e2142cddf
Ahmed Riza <dr.ahmed_riza@yahoo.co.uk>
2018-04-29 19:47:45 +0100

    phone numbers

--- week6/week6/src/main/scala/org/onedigit/scala/PhoneNumbers.scala
+++ week6/week6/src/main/scala/org/onedigit/scala/PhoneNumbers.scala
@@ -2,46 +2,20 @@ package org.onedigit.scala
 
 import scala.io.{BufferedSource, Source}
 
-class PhoneNumbers {
+// Assume you're given a dictionary, words, as a list of words.
+// Design a method, translate, such that
+// translate(phoneNumber)
+// produces all phrases of words that can serve as mnemonics for the
+// phone number
 
-  import PhoneNumbers._
-
-  // Assume you're given a dictionary, words, as a list of words.
-  // Design a method, translate, such that
-  // translate(phoneNumber)
-  // produces all phrases of words that can serve as mnemonics for the
-  // phone number
-
-  // Exmaple, the number "7225247386" should have mnemonic "Scala is fun"
-  // as one element of the set of solutions.
-
-  // Invert the mnemonic map to give a map from 'A' ... 'Z' to '2' ... '9'
-
-  // val charCode: Map[Char, Char] =
-
-  val charCode: Map[Char, Char] = mnemonics.foldLeft(Map[Char, Char]())(charCodeMapper)
-
-  private def charCodeMapper(map: Map[Char, Char], kv: (Char, String)): Map[Char, Char] = {
-    val (key, mnemoic) = kv
-    map ++ mnemoic.map(c => c -> key).toMap
-  }
-
-  /**
-    * Maps a word to the digit string it can represent, e.g. "Java"  -> "5282"
-    * @param word
-    * @return
-    */
-  def wordCode(word: String): String = ???
-
-  /**
-    * A map from digit strings to words that reprsent them,
-    * e.g. Java -> List("Java", "Kata", "Lava", ...)
-    * Note that a missing number should map to the empty set, e.g. "1111" -> List()
-    */
-
-}
+// Example, the number "7225247386" should have mnemonic "Scala is fun"
+// as one element of the set of solutions.
 
 object PhoneNumbers {
+
+  val in: BufferedSource = Source.fromFile("linuxwords.txt")
+
+  val words: List[String] = in.getLines().toList.filter(word => word.forall(chr => chr.isLetter))
 
   // Phone key mnemonics
   val mnemonics = Map(
@@ -55,16 +29,41 @@ object PhoneNumbers {
     '9' -> "WXYZ",
   )
 
-  val in: BufferedSource = Source.fromFile("linuxwords.txt")
+  // Invert the mnemonic map to give a map from 'A' ... 'Z' to '2' ... '9'
+  val charCode: Map[Char, Char] = mnemonics.foldLeft(Map[Char, Char]())(charCodeMapper)
+
+  private def charCodeMapper(map: Map[Char, Char], kv: (Char, String)): Map[Char, Char] = {
+    val (key, mnemoic) = kv
+    map ++ mnemoic.map(c => c -> key).toMap
+  }
+
+  // Alternative implementation of charCode
+  val charCode_ : Map[Char, Char] = for ( (digit, str) <- mnemonics; ltr <- str) yield ltr -> digit
+
+  /**
+    * Maps a word to the digit string it can represent, e.g. "Java"  -> "5282"
+    */
+  def wordCode(word: String): String = word.toUpperCase.map(charCode)
+
+  /**
+    * A map from digit strings to words that represent them,
+    * e.g. "5282" -> List("Java", "Kata", "Lava", ...)
+    * Note that a missing number should map to the empty set, e.g. "1111" -> List()
+    */
+  def wordsForNum: Map[String, Seq[String]] = words.groupBy(wordCode) withDefaultValue Nil
+
+  /**
+    * Return all the ways to encode a number as a list of words.
+    * e.g. encode("7225247386")
+    */
+  def encode(number: String): Set[List[String]] = {
+    ???
+  }
+
 
   def main(args: Array[String]): Unit = {
-    val words = in.getLines()
 
-    val phoneNumbers = new PhoneNumbers
-    println(phoneNumbers.charCode)
-
-    val tmp = "Java".map(c => phoneNumbers.charCode.get(c.toUpper))
-    println(tmp)
+    println(wordsForNum.get("5282"))
   }
 
 }
